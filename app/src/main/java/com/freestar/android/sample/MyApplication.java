529c123c6bd48a5986b0e00d685ab65a19c5fb13
kkawai <kkawai2@gmail.com>
2021-03-20 22:05:21 -0700

    added custom segment property for country code

--- app/src/main/java/com/freestar/android/sample/MyApplication.java
+++ app/src/main/java/com/freestar/android/sample/MyApplication.java
@@ -1,6 +1,8 @@
 package com.freestar.android.sample;
 
 
+import android.os.Build;
+
 import com.freestar.android.ads.AdRequest;
 import com.freestar.android.ads.FreeStarAds;
 
@@ -10,6 +12,15 @@ public class MyApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        String countyCode;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            countyCode = getResources().getConfiguration().getLocales().get(0).getCountry();
+        } else {
+            countyCode = getResources().getConfiguration().locale.getCountry();
+        }
+        FreeStarAds.setCustomSegmentProperty(this, countyCode, "true");
+
         AdRequest adRequest = new AdRequest(this);
         adRequest.addCustomTargeting("some target", "my value");
         FreeStarAds.enableLogging(true);  //set false for production or don't call
