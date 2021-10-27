979635454876c4a715b1d190d92a68102e23d266
kkawai <kkawai2@gmail.com>
2021-10-27 14:20:21 -0700

    confiant setup added. ad blocking mode is ON!

--- app/src/main/java/com/freestar/android/sample/MyApplication.java
+++ app/src/main/java/com/freestar/android/sample/MyApplication.java
@@ -3,12 +3,22 @@ package com.freestar.android.sample;
 
 import android.os.Build;
 
+import com.confiant.sdk.Confiant;
+import com.confiant.sdk.PropertyId;
+import com.confiant.sdk.Result;
+import com.confiant.sdk.Settings;
 import com.freestar.android.ads.AdRequest;
+import com.freestar.android.ads.ChocolateLogger;
 import com.freestar.android.ads.FreeStarAds;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.multidex.MultiDexApplication;
 
 public class MyApplication extends MultiDexApplication {
+
+    public static final String TAG = "MyApplication";
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -31,6 +41,71 @@ public class MyApplication extends MultiDexApplication {
         //Also try:
         //FreeStarAds.setAutomatedTestMode(FreeStarAds.AutomatedTestMode.BYPASS_ALL_ADS);
         FreeStarAds.init(this, MainActivity.API_KEY, adRequest);
+
+        setupConfiant();
+    }
+
+    private void setupConfiant() {
+        //https://freestar.atlassian.net/browse/ENG-8593
+        final @NonNull Result<PropertyId> propertyIdResult = PropertyId.from("x0Jm1cYDEbEK1qln_DqWfmhw8zk");
+        final @Nullable Settings settings;
+        switch(propertyIdResult.kind)
+        {
+            case Success:
+                assert propertyIdResult.value != null;
+                final @NonNull PropertyId propertyId = propertyIdResult.value;
+
+                /**
+                 * Passing true in the last param will force all ads to be blocked.
+                 * For testing purposes only.
+                 */
+                settings = //new Settings(propertyId);
+                new Settings
+                        (
+                                null, // enableRate, null for default
+                                propertyId, // propertyId
+                                null, // enableAdReporter, null for default
+                                true // forceBlockOnce, null for default
+                        );
+                break;
+            case Failure:
+                // TODO: handle error
+                assert propertyIdResult.error != null;
+                final @NonNull String message = "Failed to create Confiant Property Id: " + propertyIdResult.error.description();
+                ChocolateLogger.e(TAG, message);
+                settings = null;
+                assert false;
+                break;
+            default:
+                settings = null;
+                assert false;
+                break;
+        }
+
+        Confiant.initialize
+                (
+                        settings,
+                        (final @NonNull Result<Confiant> result) ->
+                        {
+                            switch(result.kind)
+                            {
+                                case Success:
+                                    assert result.value != null;
+                                    ChocolateLogger.i(TAG, "Successfully initialized Confiant SDK");
+                                    break;
+                                case Failure:
+                                    // TODO: handle error
+                                    assert result.error != null;
+                                    final @NonNull String message = "Failed to initialize Confiant SDK: " + result.error.description();
+                                    ChocolateLogger.e(TAG, message);
+                                    assert false;
+                                    break;
+                                default:
+                                    assert false;
+                                    break;
+                            }
+                        }
+                );
 
     }
 }
