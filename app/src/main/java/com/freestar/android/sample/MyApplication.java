9b431c91a302ce2adff726179aa463c4ef76ddfb
kkawai <kkawai2@gmail.com>
2021-12-03 15:14:19 -0800

    updated dependencies

--- app/src/main/java/com/freestar/android/sample/MyApplication.java
+++ app/src/main/java/com/freestar/android/sample/MyApplication.java
@@ -2,13 +2,18 @@ package com.freestar.android.sample;
 
 
 import android.os.Build;
+import android.util.Log;
 
 import com.freestar.android.ads.AdRequest;
+import com.freestar.android.ads.AppOpenAdListener;
 import com.freestar.android.ads.FreeStarAds;
 
 import androidx.multidex.MultiDexApplication;
 
 public class MyApplication extends MultiDexApplication {
+
+    private static final String TAG = "MyApplication";
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -30,7 +35,35 @@ public class MyApplication extends MultiDexApplication {
         //FreeStarAds.setAutomatedTestMode(FreeStarAds.AutomatedTestMode.LIMITED_MEDIATION);
         //Also try:
         //FreeStarAds.setAutomatedTestMode(FreeStarAds.AutomatedTestMode.BYPASS_ALL_ADS);
+        FreeStarAds.requestAppOpenAds("app_open_ads_p2", true, appOpenAdListener);
         FreeStarAds.init(this, MainActivity.API_KEY, adRequest);
 
     }
+
+    private final AppOpenAdListener appOpenAdListener = new AppOpenAdListener() {
+        @Override
+        public void onAppOpenAdLoading(String placement) {
+            Log.i(TAG,"onAppOpenAdLoading " + placement);
+        }
+
+        @Override
+        public void onAppOpenAdLoaded(String placement) {
+            Log.i(TAG,"onAppOpenAdLoaded " + placement);
+        }
+
+        @Override
+        public void onAppOpenAdShowing(String placement) {
+            Log.i(TAG,"onAppOpenAdShowing " + placement);
+        }
+
+        @Override
+        public void onAppOpenAdDismissed(String placement) {
+            Log.i(TAG,"onAppOpenAdDismissed " + placement);
+        }
+
+        @Override
+        public void onAppOpenAdFailed(String placement, int errorCode) {
+            Log.i(TAG,"onAppOpenAdFailed " + placement);
+        }
+    };
 }
